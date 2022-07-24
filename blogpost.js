937507462a3220a2911c250be3d02c149fa861c8
clearfeather <info@clearfeather.com>
2022-07-23 20:26:47 -0500

    add blogpost.js

---
+++ blogpost.js
@@ -0,0 +1,14 @@
+const blogPost = {
+    title: 'Post Title',
+    body: 'I made a post!',
+    author: 'Mark Zuck',
+    views: 3,
+    comments: [
+        { author: 'Speil',
+        body: 'Hello' },
+        { author: 'Dog Man',
+        body: 'Great Post' }
+    ],
+    isLive: true
+}
+console.log(blogPost);
