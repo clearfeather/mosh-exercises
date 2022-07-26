09de48749390d303a4fae4f588df073e39f48961
clearfeather <info@clearfeather.com>
2022-07-26 18:01:18 -0500

    update practice-arrays

--- practice-arrays.js
+++ practice-arrays.js
@@ -31,7 +31,17 @@ console.log(numbers);
 
 numbers = [1, -1, 2, 3];
 const filtered = numbers.filter(n => n >= 0);
-const items = filtered.map(n => `<li>${n}</li>`);
-const html = `<ul>${items.join('')}</ul>`;
+let items = filtered.map(n => `<li>${n}</li>`);
 console.log(items);
+const html = `<ul>${items.join('')}</ul>`;
 console.log(html);
+items = filtered.map(n => ({ value: n }));
+console.log(items);
+
+items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({ value: n }))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value);
+
+console.log(items);
