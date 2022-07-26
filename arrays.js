c66912c949ba2ed60273294ff8d51b5214d99feb
clearfeather <info@clearfeather.com>
2022-07-25 19:54:41 -0500

    update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 // Array Practice
 
 const numbers = [1,2,3];
+const numbers2 = [7,8,9];
 
 numbers.push(4,5);
 numbers.unshift(0);
@@ -11,3 +12,19 @@ console.log(numbers.indexOf(1));
 console.log(numbers.lastIndexOf(1));
 console.log(numbers.includes(1));
 console.log(numbers.find(element => element > 1));
+
+combined = numbers.concat(numbers2);
+console.log(combined);
+
+slice = combined.slice(2,5);
+console.log(slice);
+
+for(let number of numbers)
+    console.log(number);
+
+numbers.forEach((number, index) => console.log(number, index));
+
+numbers.sort();
+console.log(numbers);
+numbers.reverse();
+console.log(numbers);
